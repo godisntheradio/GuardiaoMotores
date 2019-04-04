1d8bfb5bf51e7ec72760618110750719177e2d4f
godisntheradio <gabriel_souzadasilva@yahoo.com.br>
2019-04-04 08:34:00 -0300

    level1

--- Project/TurnWindow.gd
+++ Project/TurnWindow.gd
@@ -1,4 +1,6 @@
 extends Control
-
+export var battle_manager_path : NodePath
+var battle_manager : BattleManager
 func _ready():
-	pass 
+	battle_manager = get_node(battle_manager_path)
+	get_node("Button").connect("button_up",battle_manager,"on_end_player_turn")
