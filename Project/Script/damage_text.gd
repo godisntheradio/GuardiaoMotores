66a1f93f9f2afe2902404a86468ae251f6f1f9dc
William <31829020+williambt@users.noreply.github.com>
2019-05-30 11:33:46 -0300

    Fixed damage text appearing on wrong position

--- Project/Script/damage_text.gd
+++ Project/Script/damage_text.gd
@@ -4,15 +4,18 @@ var label : Label = null
 var force : float = 100
 
 var start = false
+var set_visible = false
 var pos : Vector2 = Vector2.ZERO
 
 func begin(dmg : int, pos : Vector2):
-	visible = true
 	label.text = str(dmg)
 	self.pos = pos
 	start = true
 
 func _integrate_forces(state):
+	if(set_visible):
+		visible = true
+		set_visible = false
 	if(start):
 		var t = state.transform
 		t.origin = pos
@@ -21,6 +24,7 @@ func _integrate_forces(state):
 		state.linear_velocity = Vector2.ZERO
 		apply_impulse(Vector2(0, 0), Vector2(rand_range(-10, 10), -2).normalized() * force)
 		gravity_scale = 5
+		set_visible = true
 
 func _ready():
 	label = get_node("Label")
